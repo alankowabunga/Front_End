f359beae1e1816c782a8f2342ccf4942e2f1f4b0
Alan <babyalan0857@gmail.com>
2023-05-25 23:21:19 +0800

    Flip complete 1120525

--- GSAP/flip.js
+++ GSAP/flip.js
@@ -2,32 +2,37 @@
 gsap.registerPlugin(Flip);
 
 // 陣列長度為 4 => 將不斷增的數字除以 4 取餘數，即可讓數字不超過 4 且不斷循環，拿來幫索引值。 
-const layouts = ['final', 'plain', 'column', 'grid'];
-var container = document.querySelector('.container');
-let index = 0; //初始索引值為 0
+let layouts = ['final', 'plain', 'column', 'grid'],
+container = document.querySelector('.container'),
+index = 0; //初始索引值為 0
 
 function nextState() {
+    console.log("執行 next state 函式")
 
     // 儲存當前狀態的陣列
-    const state = Flip.getState('.f,.l,.i,.p,.for,.gsap');
+    const state = Flip.getState('.letter,.for,.gsap',{props:"color,backgorundColor",simple:true});
 
     //邏輯順序: 1. 先移除當前的 class屬性。2. 將索引值+1 
-    container.classList.remove(layouts[index]);
-    index = (index + 1) % layouts.length;
+    container.classList.remove(layouts[index]);// remove old class
+    index = (index + 1) % layouts.length; // increment (loop back to the start if at the end)
     container.classList.add(layouts[index]); // 下一個 layout
 
 
     Flip.from(state, { // animate from the previous state
-        duration: 2,
-        ease: 'power1.inOut',
         absolute: true,
-        onEnter:(elements,animation)=>gsap.fromTo(elements,{opacity:0},{opacity:1,animation,duration:1}),
-        onLeave:elements => gsap.to(elements,{opacity:0,duration:1})
+        scale:true,
+        duration: .7,
+        ease: 'power2.inOut',
+        simple:true,
+        stagger:0.07,
+        spin:index==0,
+        onEnter:(elements,animation)=>gsap.fromTo(elements,{opacity:0},{opacity:1,delay:animation.duration()-0.1}),
+        onLeave:elements => gsap.to(elements,{opacity:0})
 
     });
 
     // 設定回到主畫面停 3.5秒，其他間隔 1.5秒
-    gsap.delayedCall((index == 0) ? 3.5 : 1.5, nextState);
+    gsap.delayedCall((index == 0) ? 3.5 : 2, nextState);
 }
 
 gsap.delayedCall(2, nextState); //第一次呼叫觸發動畫
